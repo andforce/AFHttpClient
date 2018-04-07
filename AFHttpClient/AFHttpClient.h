b7fe8b3c3ec97326a540b3bb924460da5e9fec30
andforce <86118@163.com>
2018-04-07 19:53:20 +0800

    export header

--- AFHttpClient/AFHttpClient.h
+++ AFHttpClient/AFHttpClient.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char AFHttpClientVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AFHttpClient/PublicHeader.h>
 
-
+#import "AFHttpClient.h"
